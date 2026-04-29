a7e183db8e58861c6ea97659ed0f00a924d80d3d
MDBowen <morusdbowen@gmail.com>
2026-04-30 00:19:53 +0100

    chenges

--- run_segmented_pipeline.sh
+++ run_segmented_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python run_experiment.py \
     --timesteps 200_000 \
-    --n_trials 8 \
+    --n_trials 5 \
     --only_sp100 True \
     --max_no_improvement_evals 40 \
     --eval_num 100
