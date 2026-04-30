e902f8f4a998ad45c1f0c19d811dbc52d0c06d9e
MDBowen <morusdbowen@gmail.com>
2026-04-30 16:52:42 +0100

    umm

--- run_mbrl_comparison.sh
+++ run_mbrl_comparison.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 python run_experiment.py \
     --compare_mbrl \
-    --timesteps 50_000 \
+    --timesteps 100_000 \
+    --number_of_runs 3\
+    --max_no_improvement_evals 40
     --n_trials 3 \
     --only_sp100 True \
     --dynamics_rl_start_episode 1
